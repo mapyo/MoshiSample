b6a24e2f7861cfac50e3a3d793f8007d42640aab
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-01-16 22:34:54 +0900

    Results are different between Moshi only and KotlinJsonAdapterFactory

--- app/src/test/java/com/mapyo/moshisample/ExampleUnitTest.kt
+++ app/src/test/java/com/mapyo/moshisample/ExampleUnitTest.kt
@@ -1,6 +1,8 @@
 package com.mapyo.moshisample
 
-import org.junit.Assert.assertEquals
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import org.assertj.core.api.Assertions
 import org.junit.Test
 
 /**
@@ -8,9 +10,23 @@ import org.junit.Test
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+sealed class Animal(val name: String) {
+    class Cat : Animal("cat")
+}
+
+
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun moshi() {
+        val cat = Animal.Cat()
+
+        val moshiCatJson = Moshi.Builder().build()
+                .adapter(Animal.Cat::class.java).toJson(cat)
+        Assertions.assertThat(moshiCatJson).isEqualTo("{\"name\":\"cat\"}")
+
+        val moshiKotlinCatJson = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                .adapter(Animal.Cat::class.java).toJson(cat)
+        Assertions.assertThat(moshiKotlinCatJson).isEqualTo("{}")
     }
 }
